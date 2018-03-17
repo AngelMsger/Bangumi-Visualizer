fef4b088828fc019c008c77e840f5006e27bddc1
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-03-17 18:13:14 +0800

    Finish Author Module.

--- public/javascripts/bangumi.js
+++ public/javascripts/bangumi.js
@@ -1,13 +1,39 @@
 Vue.use(VueMaterial.default);
 
+Vue.filter('filter_slice', function (value) {
+    return value.length > 32 ? value.slice(0, 32) + '...' : value;
+});
+
 let vm = new Vue({
     el: '#app',
     data: {
         page: 0,
         menuVisible: false,
+        season_id: '',
+        anime: {
+            success: false,
+            alias: '',
+            area: [],
+            danmaku_count: 0,
+            episodes: 0,
+            evaluate: '',
+            favorites: 0,
+            is_finish: false,
+            pub_time: null,
+            rating: {
+                count: 0,
+                score: 0
+            },
+            tags: [],
+            title: '',
+            last_long_reviews_cursor: null,
+            last_short_reviews_cursor: null,
+            top_matches: [],
+            archives: []
+        },
         mid: '',
         author: {
-            avatar_url: '',
+            success: false,
             reviews: [],
             uname: '',
             follow: [],
@@ -15,40 +41,107 @@ let vm = new Vue({
             last_analyze: null,
             top_matches: [],
             recommendation: []
-        },
-        season_id: '',
-        anime: {
-
         }
     },
     watch: {
+        season_id: function () {
+            this.$http.get('/api/anime/season_id/' + this.season_id).then(response => {
+                let anime = response.body;
+                if (anime && typeof anime !== 'string') {
+                    this.anime.success = true;
+                    this.anime.alias = anime.alias;
+                    this.anime.danmaku_count = anime.danmaku_count;
+                    this.anime.episodes = anime.episodes;
+                    this.anime.evaluate = anime.evaluate;
+                    this.anime.favorites = anime.favorites;
+                    this.anime.is_finish = anime.is_finish;
+                    this.anime.pub_time = anime.pub_time;
+                    this.anime.rating = anime.rating;
+                    this.anime.title = anime.title;
+
+                    const that = this;
+
+                    while (this.anime.area.length > 0) this.anime.area.pop();
+                    if (anime.area) {
+                        anime.area.forEach(function (area) {
+                            that.anime.area.push(area.name);
+                        });
+                    }
+
+                    while (this.anime.tags.length > 0) this.anime.tags.pop();
+                    if (anime.tags) {
+                        anime.tags.forEach(function (tag) {
+                            that.anime.tags.push(tag.name);
+                        });
+                    }
+
+                    while (this.anime.top_matches.length > 0) this.anime.top_matches.pop();
+                    if (anime.top_matches) {
+                        anime.top_matches.forEach(function (pair) {
+                            that.$http.get('/api/anime/media_id/' + pair.media_id).then(response => {
+                                let other = response.body;
+                                other.similarity = pair.similarity;
+                                that.anime.top_matches.push(other)
+                            }, response => {
+                            });
+                        });
+                    }
+
+                    // this.$http.get('/api/archives/season_id/' + this.season_id).then(response => {
+                    //     that.anime.archives = response.body;
+                    // }, response => {
+                    //     that.anime.archives = [];
+                    // });
+                }
+            }, response => {
+                this.anime.success = false;
+            });
+        },
         mid: function () {
-            this.$http.get('/api/authors/' + this.mid).then(response => {
+            this.$http.get('/api/authors/mid/' + this.mid).then(response => {
                 let author = response.body;
-                this.author.reviews = author.reviews;
-                this.author.uname = author.uname;
-                this.author.follow = author.follow;
-                this.author.last_crawl = author.last_crawl;
-                this.author.last_analyze = author.last_analyze;
+                if (author && typeof author !== 'string') {
+                    console.log('author got success');
+                    this.author.success = true;
+                    this.author.reviews = author.reviews;
+                    this.author.uname = author.uname;
+                    this.author.follow = author.follow;
+                    this.author.last_crawl = author.last_crawl;
+                    this.author.last_analyze = author.last_analyze;
 
-                const that = this;
+                    const that = this;
 
-                this.author.top_matches = [];
-                author.top_matches.forEach(function (pair) {
-                    that.$http.get('/api/authors/' + pair.mid).then(response => {
-                        let other = response.body;
-                        other.similarity = pair.similarity;
-                        that.author.top_matches.push(other)
-                    }, response => {});
-                });
+                    while (this.author.top_matches.length > 0) this.author.top_matches.pop();
+                    if (author.top_matches) {
+                        author.top_matches.forEach(function (pair) {
+                            that.$http.get('/api/authors/mid/' + pair.mid).then(response => {
+                                let other = response.body;
+                                other.similarity = pair.similarity;
+                                that.author.top_matches.push(other)
+                            }, response => {
+                            });
+                        });
+                    }
 
-                this.author.recommendation = [];
-                author.recommendation.forEach(function (media_id) {
-                    that.$http.get('/api/media/' + media_id).then(response => {
-                        that.author.recommendation.push(response.body)
-                    }, response => {});
-                });
-            }, response => {});
+                    while (this.author.recommendation.length > 0) this.author.recommendation.pop();
+                    if (author.recommendation) {
+                        author.recommendation.forEach(function (media_id) {
+                            that.$http.get('/api/anime/media_id/' + media_id).then(response => {
+                                that.author.recommendation.push(response.body)
+                            }, response => {
+                            });
+                        });
+                    }
+                }
+            }, response => {
+                this.author.success = false;
+            });
+        },
+    },
+    methods: {
+        applyAnime: function (season_id) {
+            this.season_id = season_id;
+            this.page = 0;
         }
     }
 });
--- routes/api.js
+++ routes/api.js
@@ -4,28 +4,31 @@ var db = require('../db');
 var router = express.Router();
 
 /* GET api listing. */
-router.get('/season/:season_id', function(req, res) {
+router.get('/anime/season_id/:season_id', function(req, res) {
+    console.log('query' + req.params.season_id);
     db.animes.findOne({season_id: req.params.season_id}, function (error, anime) {
         if (error === null) res.send(JSON.stringify(anime));
     });
 });
 
-router.get('/media/:media_id', function(req, res) {
+router.get('/anime/media_id/:media_id', function(req, res) {
     db.animes.findOne({media_id: req.params.media_id}, function (error, anime) {
         if (error === null) res.send(JSON.stringify(anime));
     });
 });
 
-router.get('/authors/:mid', function (req, res) {
+router.get('/authors/mid/:mid', function (req, res) {
    db.authors.findOne({mid: req.params.mid}, function (error, author) {
        if (error === null) res.send(JSON.stringify(author));
    });
 });
 
-router.get('/archives/:season_id', function (req, res) {
-    db.archives.aggregate([{$replaceRoot: {"newRoot": {$mergeObjects: [{"date": "$date"}, {$arrayElemAt: [{$filter:
-                            {input: "$archives", cond: {$eq: ["$$this.season_id", req.param.season_id]}}}, 0]}]}}}],
-        function (error, archives) { if (error === null) res.send(JSON.stringify(archives)) });
+router.get('/archives/season_id/:season_id', function (req, res) {
+    db.archives.aggregate([{$replaceRoot: {newRoot: {$mergeObjects: [{date: '$date'}, {$arrayElemAt: [{$filter:{input:
+                                    '$archives', cond: {$eq: ['$$this.season_id', req.params.season_id]}}}, 0]}]}}}],
+        function (error, archives) {
+        if (error === null) res.send(JSON.stringify(archives));
+    });
 });
 
 module.exports = router;
