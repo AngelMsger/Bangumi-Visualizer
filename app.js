109d3c7204a747b241ea0495ea3c689b6862fa81
Hawking Chyi <angelmsger@AngelMsger-Development.localdomain>
2018-05-08 19:09:39 +0800

    Add CORS Support Embedded.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const conf = require('./conf');
 const index = require('./routes/index');
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use(conf.root, index);
 
